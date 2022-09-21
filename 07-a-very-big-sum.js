bedb881cb763a432bfbe9fabc17228a951d45fb8
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-21 11:45:43 -0300

    refactor: Implementando a função novamente usando foreach

--- 07-a-very-big-sum.js
+++ 07-a-very-big-sum.js
@@ -1,15 +1,20 @@
 const array = [1, 2, 3, 4, 5];
-
-// Reduce
+//Foreach
 function aVeryBigSum(ar) {
-  // Write your code here
-  let sum = ar.reduce((prev, curr) => prev + curr)
+  let sum = 0;
+  ar.forEach(element => sum += element);
   return sum;
 }
 
+
+// Reduce
+/* function aVeryBigSum(ar) {
+  let sum = ar.reduce((prev, curr) => prev + curr)
+  return sum;
+} */
+
 // Loop For
 /* function aVeryBigSum(ar) {
-  // Write your code here
   let sum = 0;
   for (let index = 0; index < ar.length; index += 1) {
     sum += ar[index];
