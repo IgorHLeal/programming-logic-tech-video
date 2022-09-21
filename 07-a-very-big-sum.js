f2d3f7b9ffade1a09ef2021f72f7520ceb9e4b0e
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-21 11:42:24 -0300

    refactor: Implementando a função novamente usando reduce

--- 07-a-very-big-sum.js
+++ 07-a-very-big-sum.js
@@ -1,12 +1,20 @@
 const array = [1, 2, 3, 4, 5];
 
+// Reduce
 function aVeryBigSum(ar) {
+  // Write your code here
+  let sum = ar.reduce((prev, curr) => prev + curr)
+  return sum;
+}
+
+// Loop For
+/* function aVeryBigSum(ar) {
   // Write your code here
   let sum = 0;
   for (let index = 0; index < ar.length; index += 1) {
     sum += ar[index];
   }
   return sum;
-}
+} */
 
 console.log(aVeryBigSum(array));
